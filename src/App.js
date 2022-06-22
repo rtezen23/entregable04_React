5a3064f1771b8c38c71563b513e8daab3efe27c3
rtezen23 <raul_tezen@hotmail.com>
2022-06-22 18:08:57 -0500

    entregable04 checked

--- src/App.js
+++ src/App.js
@@ -10,58 +10,58 @@ function App() {
   const [users, setUsers] = useState([]);
   const [userSelected, setUserSelected] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get('https://users-crud1.herokuapp.com/users/')
-    .then(res=>setUsers(res.data))
-    .catch(error=>console.log(error.response));
-  },[]);
+      .then(res => setUsers(res.data))
+      .catch(error => console.log(error.response));
+  }, []);
 
-  const getUser = () =>{
+  const getUser = () => {
     axios.get('https://users-crud1.herokuapp.com/users/')
-    .then(res=>setUsers(res.data))
-    .catch(error=>console.log(error.response));
+      .then(res => setUsers(res.data))
+      .catch(error => console.log(error.response));
   }
 
-  const createUser = newUser =>{
+  const createUser = newUser => {
     axios.post('https://users-crud1.herokuapp.com/users/', newUser)
-    .then(()=>{
-      alert('Usuario agregado');
-      getUser();
-    })
-    .catch(error=>console.log(error.response));
+      .then(() => {
+        alert('Usuario agregado');
+        getUser();
+      })
+      .catch(error => console.log(error.response));
   }
 
-  const deleteUser = userId =>{
+  const deleteUser = userId => {
     axios.delete(`https://users-crud1.herokuapp.com/users/${userId}/`)
-    .then(()=>{
-      alert('Usuario eliminado');
-      getUser();
-    })
-    .catch(error=>console.log(error.response));
+      .then(() => {
+        alert('Usuario eliminado');
+        getUser();
+      })
+      .catch(error => console.log(error.response));
   }
 
-  const selectUser = userClicked =>{
+  const selectUser = userClicked => {
     setUserSelected(userClicked);
   }
 
-  const editUser = userToEdit =>{
+  const editUser = userToEdit => {
     axios.put(`https://users-crud1.herokuapp.com/users/${userSelected.id}/`, userToEdit)
-    .then(()=>{
-      alert('Usuario editado');
-      getUser();
-    }).catch(error=>console.log(error.response));
+      .then(() => {
+        alert('Usuario editado');
+        getUser();
+      }).catch(error => console.log(error.response));
   }
 
-  const[showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <div className="App">
       <header className='header-container'>
-        <h1 className='header-title'>Users</h1>
-        <button onClick={()=>setShowForm(true)} className='header-button'>+ Create user</button>
+        <h1 className='header-title'>Users API</h1>
+        <button onClick={() => setShowForm(true)} className='header-button'>+ Create user</button>
       </header>
-      { showForm && <UsersForm createUser={createUser} userSelected={userSelected} editUser={editUser} closeForm={()=>setShowForm(false)}/>}
-      <UserList users={users} deleteUser={deleteUser} selectUser={selectUser} showForm={()=>setShowForm(true)}/>
+      {showForm && <UsersForm createUser={createUser} userSelected={userSelected} editUser={editUser} closeForm={() => setShowForm(false)} />}
+      <UserList users={users} deleteUser={deleteUser} selectUser={selectUser} showForm={() => setShowForm(true)} />
     </div>
   );
 }
